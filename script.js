1f5d696b65e2eaa53277ec377889dcefd3462eba
Justus <galaxycheesemc@gmail.com>
2022-12-06 18:41:31 -0700

    Added Current Date

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var setToday = $('#currentDay');
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +22,22 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var today = new Date();
+  var day = today.getDay();
+  var month = today.getMonth();
+  var date = today.getDate();
+  var monthNames =["January","February","March","April","May","June","July","August","September","October","November","December"]
+  var dayNames = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+
+  if (date == 1 || date == 21|| date == 31){
+  setToday.text(dayNames[day] + ", " + monthNames[month] + " " + date + "st");}
+  else if(date == 2 || date == 22){
+    setToday.text(dayNames[day] + ", " + monthNames[month] + " " + date + "nd");
+  }
+  else if(date == 3 || date == 23){
+    setToday.text(dayNames[day] + ", " + monthNames[month] + " " + date + "rd");
+  }
+  else{
+    setToday.text(dayNames[day] + ", " + monthNames[month] + " " + date + "th");
+  }
 });
